d51c19500b640951775d5395032570a26b7bdc88
folka2134 <folka2134@gmail.com>
2021-02-24 17:39:30 +0000

    Updates

---
+++ precache-manifest.ab87f0b0a7dbf2e7602ebd6a4f209772.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f211c25e0701c21f0860bdd17181fd6",
+    "url": "/kingshifi/index.html"
+  },
+  {
+    "revision": "d42ccc2023c61a0eace8",
+    "url": "/kingshifi/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "2fabe7f6380acd19e255",
+    "url": "/kingshifi/static/js/2.6e24812d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kingshifi/static/js/2.6e24812d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d42ccc2023c61a0eace8",
+    "url": "/kingshifi/static/js/main.60b26fbc.chunk.js"
+  },
+  {
+    "revision": "b25f70a834066913b5d1",
+    "url": "/kingshifi/static/js/runtime-main.fcd0a054.js"
+  }
+]);
